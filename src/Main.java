70cbe835853bc9c312a1c36a1490526c21e63348
4c <4c@INF05>
2022-04-27 11:54:54 +0200

    versione 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-
+/**
+ * 
+ * @author edoardo
+ * 
+ * @version 2
+ * 
+ * */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("prova 2");
 	}
 
 }
